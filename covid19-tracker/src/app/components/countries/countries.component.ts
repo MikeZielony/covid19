773586348a0cda81908a627412fb738fb87ccc1b
MikeZielony <51535787+MikeZielony@users.noreply.github.com>
2020-10-17 10:23:12 +0200

    Add components

--- covid19-tracker/src/app/components/countries/countries.component.ts
+++ covid19-tracker/src/app/components/countries/countries.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataServiceService} from "../../services/data-service.service";
+import {GlobalDataSummary} from "../../models/global-data";
 
 @Component({
   selector: 'app-countries',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
+  data : GlobalDataSummary[];
+  countries : string[] = [];
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
 
-  constructor() { }
+  constructor(private service : DataServiceService) { }
 
   ngOnInit(): void {
+    this.service.getGlobalData().subscribe(result=>{
+      this.data = result;
+      this.data.forEach(cs=>{
+        this.countries.push(cs.country)
+      })
+    })
+
+    }
+  updateValues(country : string){
+    console.log(country);
+    this.data.forEach(cs=>{
+      if(cs.country == country){
+        this.totalActive = cs.active
+        this.totalDeaths = cs.deaths
+        this.totalConfirmed = cs.confirmed
+        this.totalRecovered = cs.recovered
+      }
+    })
   }
 
 }
